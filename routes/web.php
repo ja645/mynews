cd60bccc3aaae0279dd200074b05411bec704313
Okayama <tbzgkzejyk1ef5s91wuf@gmail.com>
2021-02-03 06:58:57 +0000

    PHP/Laravel 09 Routingについて理解する

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/ceate', 'Admin\NewsController@add');
+    
+    //課題4.
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
+});
+
+//課題3.
+Route::get('XXX', 'Admin\AAAController@bbb');
+
+
