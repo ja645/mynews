86f90524355aa3c4b4e8e36583629f11033f9541
Okayama <tbzgkzejyk1ef5s91wuf@gmail.com>
2021-02-05 13:37:07 +0000

    PHP/Laravel 13 ニュース投稿画面を作成しよう

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -16,6 +16,39 @@
         <div class="row">
           <div class="col-md-8 mx-auto">
             <h2>Myプロフィール</h2>
+            <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+              @if (count($errors) > 0)
+              <ul>
+                @foreach($errors->all() as $e)
+                <li>{{ $e }}</li>
+                @endforeach
+              </ul>
+              @endif
+              <div class="form-group row">
+                <label class="col-md-2">氏名</label>
+                <div class="col-md-10">
+                  <input type="text" class="form-control" name="name" value="{{ old('title') }}">
+                </div>
+              </div>
+              <div class="form-group row">
+                <label class="col-md-2">性別</label>
+                <div class="col-md-10">
+                  <input type="text" class="form-control" name="gender" value="{{ old('title') }}">
+                </div>
+              </div>
+              <div class="form-group row">
+                <label class="col-md-2">趣味</label>
+                <div class="col-md-10">
+                  <input type="text" class="form-control" name="hobby" value="{{ old('title') }}">
+                </div>
+              </div>
+              <div class="form-group row">
+                <label class="col-md-2">自己紹介</label>
+                <div class="col-md-10">
+                  <textarea type="text" class="form-control" name="introduction" rows="20">{{ old('title') }}</textarea>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
       </div>
