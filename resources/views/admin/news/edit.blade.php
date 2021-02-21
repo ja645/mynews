2073d0bf63ba5cb92376914ac51b2a6acc1535b0
Okayama <tbzgkzejyk1ef5s91wuf@gmail.com>
2021-02-21 07:11:23 +0000

    news.editページを移植

--- resources/views/admin/news/edit.blade.php
+++ resources/views/admin/news/edit.blade.php
@@ -2,65 +2,50 @@
 @section('title', 'ニュースの編集')
 
 @section('content')
-  <div class="container">
-    <div class="row">
-      <div class="col-md-8 mx-auto">
-        <h2>ニュース編集</h2>
-        <form action="{{ action('Admin\NewsController@update') }}" method="post" enctype="multipart/form-data">
-          @if (count($errors) > 0)
-            <ul>
-              @foreach($errors->all() as $e)
-                <li>{{ $e }}</li>
-              @endforeach
-            </ul>
-          @endif
-          <div class="form-group row">
-            <label class="col-md-2" for="title">タイトル</label>
-            <div class="col-md-10">
-              <input type="text" class="form-control" name="title" value="{{ $news_form->title }}">
-            </div>
-          </div>
-          <div class="form-group row">
-            <label class="col-md-2" for="title">本文</label>
-            <div class="col-md-10">
-              <textarea class="form-control" name="body" row="20">{{ $news_form->body }}</textarea>
-            </div>
-          </div>
-          <div class="form-group row">
-            <label class="col-md-2" for="title">画像</label>
-            <div class="col-md-10">
-              <input type="file" class="form-control-file" name="image">
-              <div class="form-text text-info">設定中: {{ $news_form->image_path }}</div>
-            </div>
-            <div class="form-check">
-              <label class="form-check-label">
-                <input type="checkbox" class="form-check-input" name="remove" value="true">画像を削除
-              </label>
-            </div>
-          </div>
-          <div class="form-group row">
-            <div class="col-md-10">
-              <input type="hidden" name="id" value="{{ $news_form->id }}">
-              {{ csrf_field() }}
-              <input type="submit" class="btn btn-primary" value="更新">
-            </div>
-          </div>
-        </form>
-         <div class="row mt-5">
-            <div class="col-md-4 mx-auto">
-              <h2>編集履歴</h2>
-              <ul class="list-group">
-                @if ($news_form->histories != NULL)
-                  @foreach ($news_form->histories as $history)
-                    @if ($history->news_id != 0)
-                      <li class="list-group-item">{{ $history->edited_at }}</li>
-                    @endif
-                  @endforeach
-                @endif
-              </ul>
-            </div>
-          </div>
+  <div class="main">
+    
+    <h1>ニュース編集</h1>
+    
+    <form action="{{ action('Admin\NewsController@update') }}" method="post" enctype="multipart/form-data">
+      
+      <div class="news-form">
+        <input type="text" class="news-title" name="title" value="{{ $news_form->title }}" placeholder="title">
       </div>
-    </div>
+      
+      <div class="news-form">
+        <textarea class="auto_resize" type="text" name="body" rows="20" placeholder="text">{{ $news_form->body }}</textarea>
+      </div>
+      
+      <div id="img_upload">
+        <i class="fas fa-camera fa-5x"></i>
+        <input id="img_upload_form" type="file" name="image">
+        <div class="form-text text-info">設定中: {{ $news_form->image_path }}</div>
+      </div>
+      
+      <div class="form-check">
+        <label class="form-check-label">
+          <input type="checkbox" class="form-check-input" name="remove" value="true">画像を削除
+        </label>
+      </div>
+        
+      <input type="hidden" name="id" value="{{ $news_form->id }}">
+      {{ csrf_field() }}
+      
+      <div class="btn-create">
+        <input type="submit" value="Release!">
+      </div>
+    </form>
+         
+    <h2>編集履歴</h2>
+    <ul class="list-group">
+      @if ($news_form->histories != NULL)
+        @foreach ($news_form->histories as $history)
+          @if ($history->news_id != 0)
+            <li class="list-group-item">{{ $history->edited_at }}</li>
+          @endif
+        @endforeach
+      @endif
+    </ul>
+    
   </div>
 @endsection
